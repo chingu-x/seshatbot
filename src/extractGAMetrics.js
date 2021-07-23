d63970507e26bc835ebc0118bc6f821acbdf71b7
jdmedlock <jdmedlock@gmail.com>
2021-07-23 09:22:02 -0500

    Feature: Add Google Analytics extraction function [WIP]

--- src/extractGAMetrics.js
+++ src/extractGAMetrics.js
@@ -1,6 +1,40 @@
+import { BetaAnalyticsDataClient } from '@google-analytics/data'
+
+// Get Metrics from Google Analytics for a specific range of dates
+const getMetricsByRange = async (startDate, endDate) => {
+  const propertyId = `${ process.env.GA_PROPERTY_ID }`
+  // `export GOOGLE_APPLICATION_CREDENTIALS=/Users/jim/Development/sandbox/src/gacredentials.json`
+  const analyticsDataClient = new BetaAnalyticsDataClient()
+  const [response] = await analyticsDataClient.runReport({
+    property: `properties/${ process.env.GA_PROPERTY_ID }`,
+    dateRanges: [
+      {
+        startDate: '2020-10-01',
+        endDate: 'today',
+      },
+    ],
+    dimensions: [
+      {
+        name: 'eventName',
+      },
+    ],
+    metrics: [
+      {
+        name: 'eventCount',
+      },
+    ],
+  })
+
+  console.log('Report result:')
+  response.rows.forEach(row => {
+    console.log(row.dimensionValues[0], row.metricValues[0])
+  })
+}
+
 // Extract Google Analytics metrics for the chingu.io website
 const extractGAMetrics = async (environment) => {
-  console.log('here')
+  console.log('I am here')
+  await getMetricsByRange('2020-10-01', '2021-07-22')
 }
 
 export default extractGAMetrics
