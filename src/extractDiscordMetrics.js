d0b6ccb0706ef07870e833d78c0470c429af2f96
jdmedlock <jdmedlock@gmail.com>
2023-03-07 14:19:14 -0600

    Fix: Create empty message summary entries for skipped teams

--- src/extractDiscordMetrics.js
+++ src/extractDiscordMetrics.js
@@ -30,14 +30,12 @@ const getTierName = (channelName) => {
   const tierNameIndex = tierNameTranslations.findIndex(translation => 
     translation.animalName.toLowerCase() === channelName.split('-')[0].toLowerCase()
   )
-  console.log(`channelName: ${ channelName } tierNameIndex: ${ tierNameIndex } .tierName: ${ tierNameTranslations[tierNameIndex].tierName }`)
   return tierNameTranslations[tierNameIndex].tierName
 }
 
 // Extract the team number from the Discord channel name. Channel names must be
 // formatted as `<tier-name>-team-<team-no>`
 const getTeamNo = (channelName) => {
-  console.log(`channelName: ${ channelName } team no.: ${ channelName.split('-')[2] }`)
   return parseInt(channelName.split('-')[2])
 }
 
@@ -96,6 +94,7 @@ const extractDiscordMetrics = async (environment) => {
       let messageSummary = [[]] // Six sprints within any number of teams with the first cell in each being unused
       const schedule = await getVoyageSchedule(VOYAGE)
 
+      let priorTeamNo = 1
       for (let channelInfo of teamChannels) {
         const channel = channelInfo.channel
         if (channel.type !== 'category') {
@@ -106,8 +105,15 @@ const extractDiscordMetrics = async (environment) => {
           // Start by formatting the current team row with an entry for each 
           // sprint. Incoming messages will be tallied here.
           let teamNo = getTeamNo(channel.name)
-          console.log(`teamNo: ${ teamNo }`)
-          messageSummary.push([]) // Create a new row for the team
+          const gapInTeamNos = teamNo - priorTeamNo
+          if (gapInTeamNos === 0) {
+            messageSummary.push([]) // Create a new row for the team
+          } else {
+            for (let i = priorTeamNo + 1; i <= teamNo; i++) {
+              messageSummary.push([]) // Create a new row for the skipped team(s) and the current team
+            }
+          }
+
           for (let sprintNo = 0; sprintNo < 7; ++sprintNo) {
             messageSummary[teamNo].push({ 
               voyage: VOYAGE,
@@ -119,6 +125,7 @@ const extractDiscordMetrics = async (environment) => {
               userMessages: new Map()
             })
           }
+          priorTeamNo = teamNo
 
           await discordIntf.fetchAllMessages(channel, schedule, teamNo, summarizeMessages, messageSummary)
         }
